444d321a2ec6d90a2c6715e0c46ea7617ccb5a58
Jake Lawrence <jakelawrence@Jakes-MacBook-Air.local>
2022-11-11 21:16:07 -0800

    Add get endpoint for vehicles

--- index.js
+++ index.js
@@ -3,58 +3,45 @@ const express = require("express");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("./database/database.db");
 
-db.run(
-  "CREATE TABLE IF NOT EXISTS vehicle (id INTEGER PRIMARY KEY, make varchar(255) UNIQUE NOT NULL);"
-);
+db.run("CREATE TABLE IF NOT EXISTS vehicle (id INTEGER PRIMARY KEY, make varchar(255) UNIQUE NOT NULL);");
 
 const app = express();
 app.use(express.json());
 
 app.get("/vehicles/:id", (req, res, next) => {
-  console.log("here");
   db.serialize(() => {
-    db.get(
-      "SELECT make MAKE FROM vehicle WHERE make =?",
-      [req.params.id],
-      function (err, row) {
-        if (err) {
-          res.send("Error encountered while displaying");
-          return console.error(err.message);
-        }
-        res.send(`Name: ${row.MAKE}`);
-        console.log("Entry displayed successfully");
+    db.get("SELECT id, make FROM vehicle WHERE make =?", [req.params.id], function (err, row) {
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
       }
-    );
+      res.json({
+        id: row.id,
+        make: row.make,
+      });
+    });
   });
 });
 app.get("/drivers/:id", (req, res, next) => {});
 
 app.post("/vehicles", async (req, res, next) => {
   db.serialize(() => {
-    db.run(
-      "INSERT INTO vehicle (make) VALUES(?)",
-      [req.body.make],
-      function (err) {
-        if (err) {
-          res.status(500).send(err.message);
-        } else {
-          db.get(
-            "SELECT id, make FROM vehicle WHERE make =?",
-            [req.body.make],
-            function (err, row) {
-              console.log(row);
-              if (err) {
-                res.send("Error encountered while displaying");
-              }
-              res.json({
-                id: row.id,
-                make: row.make,
-              });
-            }
-          );
-        }
+    db.run("INSERT INTO vehicle (make) VALUES(?)", [req.body.make], function (err) {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        db.get("SELECT id, make FROM vehicle WHERE make =?", [req.body.make], function (err, row) {
+          console.log(row);
+          if (err) {
+            res.send("Error encountered while displaying");
+          }
+          res.json({
+            id: row.id,
+            make: row.make,
+          });
+        });
       }
-    );
+    });
   });
 });
 
